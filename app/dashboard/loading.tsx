965dae7de82cc0440755520d0df5a976c5c9ac85
BD235 <bdicky0419@gmail.com>
2025-05-14 01:59:03 +0700

    add Streaming

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,4 +1,5 @@
+import DashboardSkeleton from '@/app/ui/skeletons';
+ 
 export default function Loading() {
-    return <div className="p-4 text-center text-gray-500">Memuat data dashboard...</div>;
-  }
-  
+  return <DashboardSkeleton />;
+}
